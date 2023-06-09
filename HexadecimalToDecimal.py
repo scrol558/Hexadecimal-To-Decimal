5673fdbe5689ff9cb3d31612404dcec726493cf5
scrol <94870340+Gitik235@users.noreply.github.com>
2023-06-09 23:16:38 +0300

    Update and rename HexadecimalToBytes.py to HexadecimalToDecimal.py

--- HexadecimalToDecimal.py
+++ HexadecimalToDecimal.py
@@ -1,13 +1,13 @@
 import colorama
 
-def HexadecimalToBytes():
+def HexadecimalToDecimal():
     while True:
         hex_number = input(f"{colorama.Fore.CYAN}Hexadecimal number: {colorama.Fore.RESET}")
         decimal_number = int(hex_number, 16)
         print(f'Decimal number: {colorama.Fore.GREEN}{decimal_number}{colorama.Fore.RESET}')
 
 def main():
-    HexadecimalToBytes()
+    HexadecimalToDecimal()
 
 if __name__ == '__main__':
     main()
